63cf79e4947015e07509b9aa750c245731c4563f
RaphaelMun1z <raphaelmunizvarela@hotmail.com>
2025-03-03 18:51:15 -0300

    refactor: modularize forms

--- src/app/features/dashboard/components/views/table-owners/table-owners.component.ts
+++ src/app/features/dashboard/components/views/table-owners/table-owners.component.ts
@@ -1,19 +1,6 @@
-import { ChangeDetectorRef, Component, Input, OnInit, ViewChild } from '@angular/core';
-import { ConfirmationService, MessageService } from 'primeng/api';
-import { TableModule } from 'primeng/table';
-import { ButtonModule } from 'primeng/button';
-import { ToastModule } from 'primeng/toast';
-import { ToolbarModule } from 'primeng/toolbar';
-import { ConfirmDialog } from 'primeng/confirmdialog';
-import { InputTextModule } from 'primeng/inputtext';
-import { TextareaModule } from 'primeng/textarea';
-import { CommonModule } from '@angular/common';
-import { SelectModule } from 'primeng/select';
-import { FormsModule } from '@angular/forms';
-import { IconFieldModule } from 'primeng/iconfield';
-import { InputIconModule } from 'primeng/inputicon';
-import { Table } from 'primeng/table';
-import { DropdownModule } from 'primeng/dropdown';
+import { Component, Input } from '@angular/core';
+
+import { TableComponent } from '../table/table.component';
 import { Owner } from '../../../../../shared/interfaces/owner';
 
 interface Column {
@@ -22,86 +9,26 @@ interface Column {
     customExportHeader?: string;
 }
 
-interface ExportColumn {
-    title: string;
-    dataKey: string;
-}
-
 @Component({
     selector: 'app-table-owners',
-    imports: [FormsModule, ButtonModule, TableModule, SelectModule, ToastModule, ToolbarModule, InputTextModule, TextareaModule, CommonModule, DropdownModule, InputTextModule, FormsModule, IconFieldModule, InputIconModule],
-    providers: [MessageService, ConfirmationService],
+    imports: [TableComponent],
     templateUrl: './table-owners.component.html',
     styleUrl: './table-owners.component.scss'
 })
 
-export class TableOwnersComponent implements OnInit{
+export class TableOwnersComponent {
     @Input() owners: Owner[] = [];
-    selectedOwners!: Owner[] | null;
-    
-    @ViewChild('dt') dt!: Table;
-    cols!: Column[];
-    exportColumns!: ExportColumn[];
-    
-    constructor(
-        private messageService: MessageService,
-        private confirmationService: ConfirmationService,
-        private cd: ChangeDetectorRef
-    ) {}
-    
-    ngOnInit(): void {
-        this.configureTable();
-    }
-    
-    exportCSV() {
-        this.dt.exportCSV();
-    }
-    
-    configureTable() {
-        this.cd.markForCheck();
-        
-        this.cols = [
-            { field: 'id', header: 'Code', customExportHeader: 'Owner Code' },
-            { field: 'name', header: 'Name' },
-            { field: 'phone', header: 'Phone' },
-            { field: 'email', header: 'E-mail' }
-        ];
-        
-        this.exportColumns = this.cols.map((col) => ({ title: col.header, dataKey: col.field }));
-    }
-    
-    deleteSelectedOwners() {
-        this.confirmationService.confirm({
-            message: 'Você tem certeza que você quer deletar os owneres selecionados?',
-            header: 'Confirmar',
-            icon: 'pi pi-exclamation-triangle',
-            accept: () => {
-                this.owners = this.owners.filter((val) => !this.selectedOwners?.includes(val));
-                this.selectedOwners = null;
-                this.messageService.add({
-                    severity: 'success',
-                    summary: 'Operação Realizada',
-                    detail: 'Funcionários Deletados',
-                    life: 3000
-                });
-            }
-        });
-    }
-    
-    deleteOwner(owner: Owner) {
-        this.confirmationService.confirm({
-            message: 'Você tem certeza que você quer deletar ' + owner.id + '?',
-            header: 'Confirmar',
-            icon: 'pi pi-exclamation-triangle',
-            accept: () => {
-                this.owners = this.owners.filter((val) => val.id !== owner.id);
-                this.messageService.add({
-                    severity: 'success',
-                    summary: 'Operação Realizada',
-                    detail: 'Funcionário Deletado',
-                    life: 3000
-                });
-            }
-        });
-    }
+    globalFilterFields = ['id', 'name', 'phone', 'email'];
+    exportCols: Column[] = [
+        { field: 'id', header: 'Code', customExportHeader: 'Owner Code' },
+        { field: 'name', header: 'Name' },
+        { field: 'phone', header: 'Phone' },
+        { field: 'email', header: 'E-mail' }
+    ];
+    fields = [
+        { name: "Código", code: "id", type: "normal" },
+        { name: "Nome", code: "name", type: "normal" },
+        { name: "Telefone", code: "phone", type: "normal" },
+        { name: "E-mail", code: "email", type: "normal" },
+    ]
 }
--- src/app/features/dashboard/components/views/table-properties-features/table-properties-features.component.ts
+++ src/app/features/dashboard/components/views/table-properties-features/table-properties-features.component.ts
@@ -1,19 +1,6 @@
-import { ChangeDetectorRef, Component, Input, OnInit, ViewChild } from '@angular/core';
-import { ConfirmationService, MessageService } from 'primeng/api';
-import { TableModule } from 'primeng/table';
-import { ButtonModule } from 'primeng/button';
-import { ToastModule } from 'primeng/toast';
-import { ToolbarModule } from 'primeng/toolbar';
-import { ConfirmDialog } from 'primeng/confirmdialog';
-import { InputTextModule } from 'primeng/inputtext';
-import { TextareaModule } from 'primeng/textarea';
-import { CommonModule } from '@angular/common';
-import { SelectModule } from 'primeng/select';
-import { FormsModule } from '@angular/forms';
-import { IconFieldModule } from 'primeng/iconfield';
-import { InputIconModule } from 'primeng/inputicon';
-import { Table } from 'primeng/table';
-import { DropdownModule } from 'primeng/dropdown';
+import { Component, Input } from '@angular/core';
+
+import { TableComponent } from '../table/table.component';
 import { AdditionalFeature } from '../../../../../shared/interfaces/additionalFeature';
 
 interface Column {
@@ -22,88 +9,22 @@ interface Column {
     customExportHeader?: string;
 }
 
-interface ExportColumn {
-    title: string;
-    dataKey: string;
-}
-
 @Component({
     selector: 'app-table-properties-features',
-    imports: [FormsModule, ButtonModule, TableModule, SelectModule, ToastModule, ToolbarModule, InputTextModule, TextareaModule, CommonModule, DropdownModule, InputTextModule, FormsModule, IconFieldModule, InputIconModule],
-    providers: [MessageService, ConfirmationService],
+    imports: [TableComponent],
     templateUrl: './table-properties-features.component.html',
     styleUrl: './table-properties-features.component.scss'
 })
 
-export class TablePropertiesFeaturesComponent implements OnInit{
+export class TablePropertiesFeaturesComponent {
     @Input() additionalFeatures: AdditionalFeature[] = [];
-    selectedAdditionalFeatures!: AdditionalFeature[] | null;
-    
-    @ViewChild('dt') dt!: Table;
-    cols!: Column[];
-    exportColumns!: ExportColumn[];
-
     globalFilterFields = ['id', 'feature'];
-    
-    constructor(
-        private messageService: MessageService,
-        private confirmationService: ConfirmationService,
-        private cd: ChangeDetectorRef
-    ) {}
-    
-    ngOnInit(): void {
-        this.configureTable();
-    }
-    
-    exportCSV() {
-        this.dt.exportCSV();
-    }
-    
-    configureTable() {
-        this.cd.markForCheck();
-        
-        this.cols = [
-            { field: 'id', header: 'Code', customExportHeader: 'AdditionalFeature Code' },
-            { field: 'name', header: 'Name' },
-            { field: 'phone', header: 'Phone' },
-            { field: 'email', header: 'E-mail' }
-        ];
-        
-        this.exportColumns = this.cols.map((col) => ({ title: col.header, dataKey: col.field }));
-    }
-    
-    deleteSelectedAdditionalFeatures() {
-        this.confirmationService.confirm({
-            message: 'Você tem certeza que você quer deletar os additionalFeaturees selecionados?',
-            header: 'Confirmar',
-            icon: 'pi pi-exclamation-triangle',
-            accept: () => {
-                this.additionalFeatures = this.additionalFeatures.filter((val) => !this.selectedAdditionalFeatures?.includes(val));
-                this.selectedAdditionalFeatures = null;
-                this.messageService.add({
-                    severity: 'success',
-                    summary: 'Operação Realizada',
-                    detail: 'Funcionários Deletados',
-                    life: 3000
-                });
-            }
-        });
-    }
-    
-    deleteAdditionalFeature(additionalFeature: AdditionalFeature) {
-        this.confirmationService.confirm({
-            message: 'Você tem certeza que você quer deletar ' + additionalFeature.id + '?',
-            header: 'Confirmar',
-            icon: 'pi pi-exclamation-triangle',
-            accept: () => {
-                this.additionalFeatures = this.additionalFeatures.filter((val) => val.id !== additionalFeature.id);
-                this.messageService.add({
-                    severity: 'success',
-                    summary: 'Operação Realizada',
-                    detail: 'Funcionário Deletado',
-                    life: 3000
-                });
-            }
-        });
-    }
+    exportCols: Column[] = [
+        { field: 'id', header: 'Code', customExportHeader: 'Owner Code' },
+        { field: 'feature', header: 'Feature' },
+    ];
+    fields = [
+        { name: "Código", code: "id", type: "normal" },
+        { name: "Recurso", code: "feature", type: "normal" }
+    ]
 }
