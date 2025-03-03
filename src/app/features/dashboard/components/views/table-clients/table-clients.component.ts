e3924ac7c59280eb71e513c809340c249809e2a6
RaphaelMun1z <raphaelmunizvarela@hotmail.com>
2025-03-03 17:58:07 -0300

    refactor: modularize forms

--- src/app/features/dashboard/components/views/table-clients/table-clients.component.ts
+++ src/app/features/dashboard/components/views/table-clients/table-clients.component.ts
@@ -1,20 +1,8 @@
-import { ChangeDetectorRef, Component, Input, OnInit, ViewChild } from '@angular/core';
-import { ConfirmationService, MessageService } from 'primeng/api';
-import { TableModule } from 'primeng/table';
-import { ButtonModule } from 'primeng/button';
-import { ToastModule } from 'primeng/toast';
-import { Tag } from 'primeng/tag';
-import { ToolbarModule } from 'primeng/toolbar';
-import { InputTextModule } from 'primeng/inputtext';
-import { TextareaModule } from 'primeng/textarea';
-import { CommonModule } from '@angular/common';
-import { SelectModule } from 'primeng/select';
+import { Component, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { IconFieldModule } from 'primeng/iconfield';
-import { InputIconModule } from 'primeng/inputicon';
-import { Table } from 'primeng/table';
-import { DropdownModule } from 'primeng/dropdown';
-import { Client, ClientCreate } from '../../../../../shared/interfaces/client';
+
+import { TableComponent } from '../table/table.component';
+import { Client } from '../../../../../shared/interfaces/client';
 
 interface Column {
     field: string;
@@ -22,89 +10,28 @@ interface Column {
     customExportHeader?: string;
 }
 
-interface ExportColumn {
-    title: string;
-    dataKey: string;
-}
-
 @Component({
     selector: 'app-table-clients',
-    imports: [FormsModule, ButtonModule, Tag, TableModule, SelectModule, ToastModule, ToolbarModule, InputTextModule, TextareaModule, CommonModule, DropdownModule, InputTextModule, FormsModule, IconFieldModule, InputIconModule],
-    providers: [MessageService, ConfirmationService],
+    imports: [TableComponent, FormsModule],
     templateUrl: './table-clients.component.html',
     styleUrl: './table-clients.component.scss'
 })
 
-export class TableClientsComponent implements OnInit{
+export class TableClientsComponent {
     @Input() clients: Client[] = [];
-    selectedClients!: Client[] | null;
-    
-    @ViewChild('dt') dt!: Table;
-    cols!: Column[];
-    exportColumns!: ExportColumn[];
-
-    globalFilterFields = ['id', 'name', 'phone', 'email', 'registrationDate'];
-    
-    constructor(
-        private messageService: MessageService,
-        private confirmationService: ConfirmationService,
-        private cd: ChangeDetectorRef
-    ) {}
-    
-    ngOnInit(): void {
-        this.configureTable();
-    }
-    
-    exportCSV() {
-        this.dt.exportCSV();
-    }
-    
-    configureTable() {
-        this.cd.markForCheck();
-
-        this.cols = [
-            { field: 'id', header: 'Code', customExportHeader: 'Client Code' },
-            { field: 'name', header: 'Name' },
-            { field: 'phone', header: 'Phone' },
-            { field: 'email', header: 'E-mail' },
-            { field: 'tenantStatus', header: 'Status' }
-        ];
-        
-        this.exportColumns = this.cols.map((col) => ({ title: col.header, dataKey: col.field }));
-    }
-    
-    deleteSelectedClients() {
-        this.confirmationService.confirm({
-            message: 'Você tem certeza que você quer deletar os clientes selecionados?',
-            header: 'Confirmar',
-            icon: 'pi pi-exclamation-triangle',
-            accept: () => {
-                this.clients = this.clients.filter((val) => !this.selectedClients?.includes(val));
-                this.selectedClients = null;
-                this.messageService.add({
-                    severity: 'success',
-                    summary: 'Operação Realizada',
-                    detail: 'Clientes Deletados',
-                    life: 3000
-                });
-            }
-        });
-    }
-    
-    deleteClient(client: Client) {
-        this.confirmationService.confirm({
-            message: 'Você tem certeza que você quer deletar ' + client.name + '?',
-            header: 'Confirmar',
-            icon: 'pi pi-exclamation-triangle',
-            accept: () => {
-                this.clients = this.clients.filter((val) => val.id !== client.id);
-                this.messageService.add({
-                    severity: 'success',
-                    summary: 'Operação Realizada',
-                    detail: 'Cliente Deletado',
-                    life: 3000
-                });
-            }
-        });
-    }
+    globalFilterFields = ['id', 'name', 'phone', 'email', 'tenantStatus'];
+    cols: Column[] = [
+        { field: 'id', header: 'Code', customExportHeader: 'Contract Code' },
+        { field: 'name', header: 'Name' },
+        { field: 'phone', header: 'Phone' },
+        { field: 'email', header: 'E-mail' },
+        { field: 'tenantStatus', header: 'TenantStatus' },
+    ];
+    fields = [
+        { name: "Código", code: "id", type: "normal" },
+        { name: "Nome", code: "name", type: "normal" },
+        { name: "Telefone", code: "phone", type: "normal" },
+        { name: "E-mail", code: "email", type: "normal" },
+        { name: "Status", code: "tenantStatus", type: "tag" },
+    ]
 }
---
+++ src/app/features/dashboard/components/views/table/table.component.ts
@@ -0,0 +1,101 @@
+import { AfterViewInit, ChangeDetectorRef, Component, Input, OnInit, ViewChild } from '@angular/core';
+
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { InputTextModule } from 'primeng/inputtext';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { TextareaModule } from 'primeng/textarea';
+import { DropdownModule } from 'primeng/dropdown';
+import { ToolbarModule } from 'primeng/toolbar';
+import { SelectModule } from 'primeng/select';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { ToastModule } from 'primeng/toast';
+import { Table } from 'primeng/table';
+import { Tag } from 'primeng/tag';
+import { CommonModule } from '@angular/common';
+
+interface Column {
+    field: string;
+    header: string;
+    customExportHeader?: string;
+}
+
+interface ExportColumn {
+    title: string;
+    dataKey: string;
+}
+
+@Component({
+    selector: 'app-table',
+    imports: [CommonModule, TableModule, Tag, DropdownModule, InputTextModule, TextareaModule, ToastModule, SelectModule, InputIconModule, IconFieldModule, ButtonModule, ToolbarModule],
+    providers: [MessageService, ConfirmationService],
+    templateUrl: './table.component.html',
+    styleUrl: './table.component.scss'
+})
+
+export class TableComponent implements OnInit {
+    @Input() items!: any[];
+    @Input() fields!: {name: string; code: string, type: string}[];
+    selectedItems!: any[] | null;
+    
+    @ViewChild('dt') dt!: Table;
+    @Input() cols!: Column[];
+    exportColumns!: ExportColumn[];
+    
+    @Input() globalFilterFields!: string[];
+    
+    constructor(
+        private messageService: MessageService,
+        private confirmationService: ConfirmationService,
+        private cd: ChangeDetectorRef
+    ) {}
+
+    ngOnInit(): void {
+        this.configureTable();
+    }
+    
+    exportCSV() {
+        this.dt.exportCSV();
+    }
+    
+    configureTable() {
+        this.cd.markForCheck();
+        this.exportColumns = this.cols.map((col) => ({ title: col.header, dataKey: col.field }));
+    }
+    
+    deleteSelectedItems() {
+        this.confirmationService.confirm({
+            message: 'Você tem certeza que você quer deletar os itens selecionados?',
+            header: 'Confirmar',
+            icon: 'pi pi-exclamation-triangle',
+            accept: () => {
+                this.items = this.items.filter((val) => !this.selectedItems?.includes(val));
+                this.selectedItems = null;
+                this.messageService.add({
+                    severity: 'success',
+                    summary: 'Operação Realizada',
+                    detail: 'Clientes Deletados',
+                    life: 3000
+                });
+            }
+        });
+    }
+    
+    deleteItem(item: any) {
+        this.confirmationService.confirm({
+            message: 'Você tem certeza que você quer deletar ' + item.name + '?',
+            header: 'Confirmar',
+            icon: 'pi pi-exclamation-triangle',
+            accept: () => {
+                this.items = this.items.filter((val) => val.id !== item.id);
+                this.messageService.add({
+                    severity: 'success',
+                    summary: 'Operação Realizada',
+                    detail: 'Registro Deletado',
+                    life: 3000
+                });
+            }
+        });
+    }
+}
